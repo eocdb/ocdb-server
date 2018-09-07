fb9c7a1572de5cd0a6c4d606eeca67519074a1a3
tom <tom.block@brockmann-consult.de>
2018-09-07 11:40:47 +0200

    parsing records

--- eocdb/core/seabass/sb_file_reader.py
+++ eocdb/core/seabass/sb_file_reader.py
@@ -20,18 +20,14 @@ class SbFileReader():
         self.lines = lines
 
         self.handle_header = None
-        while True:
-            line = self._next_line()
-            if '/begin_header' in line.lower():
-                self.handle_header = True
-                self._parse_header(dataset)
 
-            self._interprete_header(dataset)
+        line = self._next_line()
+        if '/begin_header' in line.lower():
+            self.handle_header = True
+            self._parse_header(dataset)
 
-            self._parse_records(dataset)
-
-            if line == EOF:
-                break
+        self._interprete_header(dataset)
+        self._parse_records(dataset)
 
         if self.handle_header is None or self.handle_header is True:
             raise IOError("/end_header tag missing")
@@ -67,14 +63,40 @@ class SbFileReader():
             # split line, trim and remove leading slash
             line = re.sub("[\r\n]+", '', line).strip()
             [key, value] = line.split('=', 1)
-            dataset.add_metadatum(key[1:].strip(), value.strip())
+            key = key[1:].strip()
+            value = value.strip()
+            if key == 'fields':
+                self.field_list = value
+            else:
+                dataset.add_metadatum(key, value)
 
     def _interprete_header(self, dataset):
         self._delimiter_regex = self._extract_delimiter_regex(dataset)
 
+        if self.field_list is None:
+            raise IOError('Missing header tag "fields"')
+
+        variable_names = self.field_list.lower().split(',')
+        dataset.add_attributes(variable_names)
+
     def _parse_records(self, dataset):
-        # @todo 1 tb/tb implement
-        pass
+        while True:
+            line = self._next_line()
+            if line == EOF:
+                break
+
+            tokens = re.split(self._delimiter_regex, line)
+            record = []
+            for token in tokens:
+                if self._is_number(token):
+                    if self._is_integer(token):
+                        record.append(int(token))
+                    else:
+                        record.append(float(token))
+                else:
+                    record.append(token)
+
+            dataset.add_record(record)
 
     def _extract_delimiter_regex(self, dataset):
         if not 'delimiter' in dataset.metadata:
@@ -89,3 +111,18 @@ class SbFileReader():
             return '\t+'
         else:
             raise IOError('Invalid delimiter-value in header')
+
+    def _is_number(self, token):
+        try:
+            float(token)
+            return True
+        except ValueError:
+            return False
+
+    def _is_integer(self, token):
+        try:
+            int(token)
+            return True
+        except ValueError:
+            return False
+
--- tests/core/seabass/test_sb_file_reader.py
+++ tests/core/seabass/test_sb_file_reader.py
@@ -43,12 +43,24 @@ class SbFileReaderTest(unittest.TestCase):
                    '!/cruise=FK160603\n',
                    '/station=03_04\n',
                    '/delimiter=comma\n',
-                   '/end_header\n']
+                   '/fields=time,depth,ED379.9,ED412.1,ED442.6,ED470.4,ED490.5,ED510.8,ED532.4,ED555.3,ED589.6,ED619.7,ED669.8,ED683.3,ED704.9,ED779.4,LU380.3,LU470.4,LU510.1,LU589.8,LU619.7,LU704.8,LU779.9,tilt,COND,Wt,pvel\n',
+                   '/end_header\n',
+                   '05:42:49,0.40000000,56.61060942,122.66957337,132.33737132,114.44906813,121.14584599,129.14107229,164.93812382,153.82678513,107.74022908,74.84475416,112.73332494,117.61951804,44.97546967,12.29104733,-999,-999,-999,-999,-999,-999,-999,-999,-999,-999,-999',
+                   '05:42:50,0.50000000,56.90948085,115.97783666,171.51381329,100.38906060,161.96556721,113.75394300,113.36437951,77.19171621,94.68081637,74.03389812,80.24165958,92.87650441,41.91937544,7.05047058,-999,-999,-999,-999,-999,-999,-999,2.32841663,53.06609583,27.46488778,0.37780480']
 
         document = self.reader._parse(sb_file)
-        self.assertEqual({'data_file_name': 'pro_03_04AA_L2s.dat', 'data_type': 'cast', 'data_status': 'preliminary', 'experiment': 'LAMONT_SCS', 'cruise': 'jun16scs', 'station': '03_04', 'delimiter': 'comma'}, document.metadata)
-        self.assertEqual(0, document.attribute_count)
-        self.assertEqual(0, document.record_count)
+        self.assertEqual({'data_file_name': 'pro_03_04AA_L2s.dat', 'data_type': 'cast', 'data_status': 'preliminary',
+                          'experiment': 'LAMONT_SCS', 'cruise': 'jun16scs', 'station': '03_04', 'delimiter': 'comma'}, document.metadata)
+
+        self.assertEqual(27, document.attribute_count)
+        self.assertEqual("time", document.attribute_names[0])
+        self.assertEqual("lu470.4", document.attribute_names[17])
+
+        self.assertEqual(2, document.record_count)
+        self.assertEqual('05:42:50', document.records[1][0])
+        self.assertAlmostEqual(56.90948085, document.records[1][2])
+        self.assertEqual(-999, document.records[1][21])
+        self.assertEqual(27, len(document.records[0]))
 
     def test_extract_delimiter_regex(self):
         dataset = DbDataset()
@@ -83,3 +95,16 @@ class SbFileReaderTest(unittest.TestCase):
             self.fail("IOException expected")
         except IOError:
             pass
+
+    def test_is_number(self):
+        self.assertTrue(self.reader._is_number('1246'))
+        self.assertTrue(self.reader._is_number('0.2376'))
+        self.assertFalse(self.reader._is_number('23:07:33'))
+        self.assertFalse(self.reader._is_number('nasenmann'))
+
+    def test_is_integer(self):
+        self.assertTrue(self.reader._is_integer('1246'))
+        self.assertTrue(self.reader._is_integer('-999'))
+        self.assertFalse(self.reader._is_integer('0.25216'))
+        self.assertFalse(self.reader._is_integer('23:07:33'))
+        self.assertFalse(self.reader._is_integer('rattelschneck'))
