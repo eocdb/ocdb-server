eff3563187ee9517d2a67c2eb0a3c1e210097d95
Norman Fomferra <norman.fomferra@gmail.com>
2018-09-07 13:05:00 +0200

    allow to use parameter "q"

--- eocdb/ws/handlers.py
+++ eocdb/ws/handlers.py
@@ -38,5 +38,5 @@ class MeasurementsQueryHandler(WsRequestHandler):
 
     def get(self):
         self.set_header('Content-Type', 'text/json')
-        query_string = self.params.get_query_argument('query', '')
+        query_string = self.params.get_query_argument('query', self.params.get_query_argument('q', ''))
         self.write(self.to_json(self.ws_context.query_measurements(query_string)))
