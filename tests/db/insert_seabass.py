3e66ec144a010bb3876885a48416b91e38352af7
tom <tom.block@brockmann-consult.de>
2018-09-21 12:43:02 +0200

    attach to db, skip too large files

--- tests/db/insert_seabass.py
+++ tests/db/insert_seabass.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from eocdb.core.seabass.sb_file_reader import SbFileReader
+from eocdb.db.mongo_db_driver import MongoDbDriver
 
 
 class InsertSeabass():
@@ -13,6 +14,13 @@ class InsertSeabass():
             raise IOError("input directory does not exist: " + input_dir)
 
         reader = SbFileReader()
+        db_driver = MongoDbDriver()
+
+        document_count = 0
+        record_count = 0
+
+        #try:
+        db_driver.connect()
 
         for root, dirs, files in os.walk(input_dir):
             if not 'archive' in root:
@@ -20,6 +28,11 @@ class InsertSeabass():
 
             for name in files:
                 full_path = os.path.join(root, name)
+
+                if os.path.getsize(full_path) > 8500000:
+                    print("SKIPPING - too large:" + full_path)
+                    continue
+
                 to_ingest = False
                 # we need to ignore errors here to prevent Python to use unicode decoders on binary files tb 2018-09-20
                 with open(full_path, errors='ignore') as file:
@@ -30,6 +43,16 @@ class InsertSeabass():
                 if to_ingest:
                     print(full_path)
                     document = reader.read(full_path)
+
+                    document_count += 1
+                    record_count += document.record_count
+
+                    db_driver.insert(document.to_dict())
+        #except:
+
+        print("Number of docs: " + str(document_count))
+        print("Number of recs: " + str(record_count))
+        db_driver.close()
 
 
 if __name__ == "__main__":
