5df0119a4d76d3ea7f6b94d418bd17f8e4eb876c
Norman Fomferra <norman.fomferra@gmail.com>
2018-10-05 21:53:13 +0200

    new openapi module that reads ws/res/openapi.yml

---
+++ eocdb/ws/openapi.py
@@ -0,0 +1,463 @@
+import importlib
+import os
+from typing import List, Any, Dict, Tuple
+
+import yaml
+
+from eocdb.ws.webservice import url_pattern
+
+_MODEL_CODE = '''
+import pprint
+from typing import Any, Dict, Type, TypeVar
+
+T = TypeVar('T')
+
+
+class Model(object):
+
+    @classmethod
+    def from_dict(cls: Type[T], d: Dict[str, Any]) -> T:
+        """Get the dictionary ``d`` as a model."""
+        # TODO deserialize model
+        return cls()
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Get the model properties as a dict."""
+        result = {}
+
+        prop_names = [k for k in self.__dict__ if hasattr(self, k) and hasattr(self, "_" + k)]
+        for prop_name in prop_names:
+            value = getattr(self, prop_name)
+            if isinstance(value, list):
+                result[prop_name] = list(map(self._to_dict, value))
+            elif hasattr(value, "to_dict"):
+                result[prop_name] = value.to_dict()
+            elif isinstance(value, dict):
+                result[prop_name] = dict(map(self._to_dict_pair, value.items()))
+            else:
+                result[prop_name] = value
+
+        return result
+
+    @classmethod
+    def _to_dict(cls, x):
+        return x.to_dict() if hasattr(x, "to_dict") else x
+
+    @classmethod
+    def _to_dict_pair(cls, x):
+        key, value = x
+        if not hasattr(value, "to_dict"):
+            return x
+        return key, value.to_dict()
+
+    def to_str(self) -> str:
+        """Returns the string representation of the model
+
+        :rtype: str
+        """
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self) -> str:
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other) -> bool:
+        """Returns true if both objects are equal"""
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other) -> bool:
+        """Returns true if both objects are not equal"""
+        return not self == other
+'''
+
+
+class OpenApi:
+
+    def __init__(self, spec_path: str, package: str):
+        file = os.path.join(os.path.dirname(__file__), spec_path.replace('/', os.path.sep))
+        with open(file) as fp:
+            spec = yaml.load(fp)
+        spec_version = spec.get("openapi")
+        if spec_version != "3.0.0":
+            raise ValueError(f"Unsupported OpenAPI version {spec_version}, must be 3.0.0")
+        self._spec = spec
+        self._package = package
+        self._operations = []
+        self._schemas = []
+        self._resolve_operations()
+        self._resolve_schemas()
+
+    @property
+    def spec(self) -> Dict[str, Any]:
+        return self._spec
+
+    @property
+    def operations(self) -> List["Operation"]:
+        return self._operations
+
+    @property
+    def schemas(self) -> List["Schema"]:
+        return self._schemas
+
+    @property
+    def path_mappings(self):
+        package = self._package + ".handlers"
+        path_mappings = []
+        for op in self._operations:
+            path_mappings.append(op.new_path_mapping(package))
+
+        return path_mappings
+
+    def gen_code(self, base_dir: str):
+        self._gen_handlers(base_dir)
+        self._gen_controllers(base_dir)
+        self._gen_models(base_dir)
+
+    def _gen_handlers(self, base_dir: str):
+        package = self._package + ".handlers"
+        module_code = dict()
+        for op in self._operations:
+            op.gen_handler_code(module_code)
+        module_code["_mappings"] = self._gen_paths_code()
+        module_code["__init__"] = ["from ._mappings import MAPPINGS\n"]
+        self._write_code(base_dir, package, module_code)
+
+    def _gen_controllers(self, base_dir: str):
+        package = self._package + ".controllers"
+        module_code = dict()
+        for op in self._operations:
+            op.gen_controller_code(module_code)
+        module_code["__init__"] = ["\n"]
+        self._write_code(base_dir, package, module_code)
+
+    def _gen_models(self, base_dir: str):
+        package = self._package + ".models"
+        module_code = dict()
+        for schema in self._schemas:
+            schema.gen_code(module_code)
+        module_code["__init__"] = ["\n"]
+        module_code['_model'] = [line + "\n" for line in _MODEL_CODE.split("\n")]
+        self._write_code(base_dir, package, module_code)
+
+    @classmethod
+    def _write_code(cls, base_dir: str, package: str, module_code: Dict[str, List[str]]):
+        package_dir = os.path.join(base_dir, *package.split("."))
+        os.makedirs(package_dir, exist_ok=True)
+        for module_name in module_code:
+            code = module_code[module_name]
+            with open(os.path.join(package_dir, module_name + ".py"), "w") as fp:
+                fp.writelines(code)
+
+    def _resolve_operations(self):
+        paths = self._spec.get("paths")
+        for path in paths:
+            path_props = paths[path]
+            self._resolve_op_method(path, path_props, "get")
+            self._resolve_op_method(path, path_props, "put")
+            self._resolve_op_method(path, path_props, "post")
+            self._resolve_op_method(path, path_props, "delete")
+
+    def _resolve_op_method(self, path: str, path_props: Dict[str, Any], method: str):
+        method_props = path_props.get(method)
+        if method_props is not None:
+            self._resolve_op_method_props(path, method, method_props)
+
+    def _resolve_op_method_props(self, path: str, method: str, method_props: Dict[str, Any]):
+        operation_id = method_props.get("operationId")
+        if not operation_id:
+            raise ValueError(f"paths/'{path}'/{method}/operationId is required")
+        tags = method_props.get("tags")
+        if not tags:
+            tags = ["default"]
+        parameters = method_props.get("parameters", [])
+        parameter_specs = []
+        for i in range(len(parameters)):
+            parameter = parameters[i]
+            parameter = self._resolve_ref(parameter, f"paths/'{path}'/{method}/parameters/{i}", expand=True)
+            if "name" not in parameter:
+                raise ValueError(f"paths/'{path}'/{method}/parameters/{i}/name is required")
+            if "in" not in parameter:
+                raise ValueError(f"paths/'{path}'/{method}/parameters/{i}/in is required")
+            if "type" not in parameter and "schema" not in parameter:
+                raise ValueError(f"paths/'{path}'/{method}/parameters/{i}/type or schema is required")
+            parameter_spec = dict(parameter)
+            parameter_specs.append(parameter_spec)
+        self._operations.append(Operation(path, tags[0], operation_id, method, parameter_specs))
+
+    def _resolve_schemas(self):
+        components = self._spec.get("components")
+        if not components or "schemas" not in components:
+            return
+        schemas = components.get("schemas")
+        if not schemas:
+            return
+
+        for schema_name in schemas:
+            schema_props = schemas[schema_name]
+            schema_type = schema_props.get("type", "object")
+            if schema_type != "object":
+                raise ValueError(f"components/schemas/{schema_name}/type must be object")
+            schema_required = schema_props.get("required", [])
+            schema_props = schema_props.get("properties", {})
+            resolved_schema_props = dict()
+            for prop_name in schema_props:
+                prop = schema_props[prop_name]
+                prop = self._resolve_ref(prop, f"components/schemas/{schema_name}", expand=False)
+                resolved_schema_props[prop_name] = prop
+            self._schemas.append(Schema(schema_name, schema_required, resolved_schema_props))
+
+    def _resolve_ref(self, d: dict, d_path: str, expand: bool = False):
+        ref = d.get("$ref")
+        if ref:
+            keys = ref.split('/')
+            if len(keys) != 4 or keys[0] != '#':
+                raise ValueError(f"invalid {d_path}/$ref: {ref}")
+            keys = keys[1:]
+            if expand:
+                d = self._spec[keys[0]][keys[1]][keys[2]]
+            else:
+                d = dict(type=keys[2])
+        return d
+
+    def _gen_paths_code(self):
+        code = list()
+        module_names = set()
+        for op in self.operations:
+            module_names.add(op.tag.lower())
+        for module_name in sorted(module_names):
+            code.append(f"from ..handlers.{module_name} import *\n")
+        code.append("from ..webservice import url_pattern\n")
+        code.append("\n")
+        code.append("\n")
+        code.append("MAPPINGS = (\n")
+        for op in self.operations:
+            class_name = _to_py_camel(op.operation_id)
+            code.append(f"    (url_pattern('{op.path}'), {class_name}),\n")
+        code.append(")\n")
+        return code
+
+
+class Operation:
+    def __init__(self, path: str, tag: str, operation_id: str, method: str, parameters: List[Dict[str, Any]]):
+        self.path = path
+        self.tag = tag
+        self.operation_id = operation_id
+        self.method = method
+        self.parameters = parameters
+
+    @property
+    def path_parameters(self):
+        return [p for p in self.parameters if p['in'] == 'path']
+
+    @property
+    def query_parameters(self):
+        return [p for p in self.parameters if p['in'] == 'query']
+
+    @property
+    def split_parameters(self) -> Tuple[List, List]:
+        required_params = list(self.path_parameters)
+        optional_params = []
+        for p in self.query_parameters:
+            required = p.get("required", False)
+            if required:
+                required_params.append(p)
+            else:
+                optional_params.append(p)
+        return required_params, optional_params
+
+    def new_path_mapping(self, package: str):
+        module_name = self.tag.lower()
+        class_name = _to_py_camel(self.operation_id)
+
+        module = importlib.import_module(package + "." + module_name)
+        handler_class = getattr(module, class_name)
+
+        return url_pattern(self.path), handler_class
+
+    def gen_handler_code(self, module_code: Dict[str, List[str]]):
+        module_name = self.tag.lower()
+        class_name = _to_py_camel(self.operation_id)
+        func_name = _to_py_lower(self.operation_id)
+
+        if module_name in module_code:
+            code = module_code[module_name]
+        else:
+            code = [f"from ..webservice import WsRequestHandler\n",
+                    f"from ..controllers.{module_name} import *\n"]
+            module_code[module_name] = code
+
+        path_params = self.path_parameters
+        query_params = self.query_parameters
+
+        path_params_str_parts = []
+        for path_param in path_params:
+            name, _, data_type, _ = _get_name_type_default(path_param, default_type="str")
+            path_params_str_parts.append(f"{name}: {data_type}")
+        path_params_str = ", ".join(path_params_str_parts)
+
+        code.append("\n")
+        code.append("\n")
+        code.append("# noinspection PyAbstractClass\n")
+        code.append(f"class {class_name}(WsRequestHandler):\n")
+        if path_params_str:
+            code.append(f"    def {self.method}(self, {path_params_str}):\n")
+        else:
+            code.append(f"    def {self.method}(self):\n")
+
+        for query_param in query_params:
+            name, py_name, data_type, default_value = _get_name_type_default(query_param, default_type="str")
+            code.append(f"        {py_name} = self.params.get_query_argument('{name}', {default_value})\n")
+
+        call_args_parts = []
+        for param in path_params:
+            name, py_name, _, _ = _get_name_type_default(param)
+            call_args_parts.append(f"{py_name}={name}")
+        for param in query_params:
+            _, py_name, _, _ = _get_name_type_default(param)
+            call_args_parts.append(f"{py_name}={py_name}")
+        call_args = ", ".join(call_args_parts)
+
+        code.append(f"        self.set_header('Content-Type', 'text/json')\n")
+        if call_args:
+            code.append(f"        return {func_name}(self.ws_context, {call_args})\n")
+        else:
+            code.append(f"        return {func_name}(self.ws_context)\n")
+
+    def gen_controller_code(self, module_code: Dict[str, List[str]]):
+        module_name = self.tag.lower()
+        func_name = _to_py_lower(self.operation_id)
+
+        if module_name in module_code:
+            code = module_code[module_name]
+        else:
+            code = ["from typing import List\n",
+                    "\n",
+                    "from ..context import WsContext\n"]
+            module_code[module_name] = code
+
+        required_params, optional_params = self.split_parameters
+
+        param_decl_parts = []
+        for path_param in required_params:
+            _, name, data_type, _ = _get_name_type_default(path_param, default_type="str")
+            param_decl_parts.append(f"{name}: {data_type}")
+        for path_param in optional_params:
+            _, name, data_type, default_value = _get_name_type_default(path_param, default_type="str")
+            param_decl_parts.append(f"{name}: {data_type} = {default_value}")
+        param_decl = ", ".join(param_decl_parts)
+
+        code.append("\n")
+        code.append("\n")
+        if param_decl:
+            code.append(f"def {func_name}(ctx: WsContext, {param_decl}):\n")
+        else:
+            code.append(f"def {func_name}(ctx: WsContext):\n")
+
+        code.append(f"    # TODO: implement operation {self.operation_id}\n")
+        code.append(f"    return dict(code=200, status='OK')\n")
+
+    def __str__(self):
+        query_params = ', '.join(p['name'] for p in self.parameters if p.get('in', '') == 'query')
+        return f"{self.path} => {self.tag}.{self.operation_id}.{self.method}({query_params})"
+
+
+class Schema:
+    def __init__(self, name: str, required: List[str], properties: Dict[str, Dict[str, Any]]):
+        self.name = name
+        self.required = required
+        self.properties = properties
+
+    def gen_code(self, module_code: Dict[str, List[str]]):
+        module_name = _to_py_lower(self.name)
+        class_name = _to_py_camel(self.name)
+
+        if module_name in module_code:
+            code = module_code[module_name]
+        else:
+            code = ["from typing import List\n",
+                    "\n",
+                    "from ._model import Model\n"]
+            module_code[module_name] = code
+
+        code.append("\n")
+        code.append("\n")
+        code.append(f"class {class_name}(Model):\n")
+
+        code.append("\n")
+        code.append(f"    def __init__(self):\n")
+        if self.properties:
+            for prop_name in self.properties:
+                prop_name = _to_py_lower(prop_name)
+                code.append(f"        self._{prop_name} = None\n")
+        else:
+            code.append(f"        pass\n")
+
+        for prop_name in self.properties:
+            prop = self.properties[prop_name]
+            _, prop_name, prop_type, _ = _get_name_type_default(prop, default_name=prop_name)
+            code.append("\n")
+            code.append(f"    @property\n")
+            code.append(f"    def {prop_name}(self) -> {prop_type}:\n")
+            code.append(f"        return self._{prop_name}\n")
+            code.append("\n")
+            code.append(f"    @{prop_name}.setter\n")
+            code.append(f"    def {prop_name}(self, value: {prop_type}):\n")
+            code.append(f"        self._{prop_name} = value\n")
+
+
+_TYPE_MAPPING = dict(string="str", number="float", boolean="bool", integer="int", array='List')
+
+
+def _to_py_type(type_name, default: str = None):
+    return _TYPE_MAPPING.get(type_name, type_name if default is None else default)
+
+
+def _to_py_lower(s: str):
+    s2 = ''
+    n = len(s)
+    for i in range(n):
+        c = s[i]
+        if c.islower() and i < n - 1 and s[i + 1].isupper():
+            s2 += c
+            s2 += "_"
+        else:
+            s2 += c.lower()
+    return s2
+
+
+def _to_py_camel(s: str):
+    n = len(s)
+    if n == 0:
+        return s
+    s2 = s[0].upper()
+    for i in range(1, n):
+        c = s[i]
+        if s[i - 1] == "_":
+            s2 += c.upper()
+        else:
+            s2 += c
+    return s2
+
+
+def _get_name_type_default(param: Dict[str, Any],
+                           default_name=None,
+                           default_type=None,
+                           default_value=None) -> Tuple[str, str, str, Any]:
+    name = param.get("name", default_name)
+    py_name = _to_py_lower(name)
+    schema = param.get("schema")
+    data_type = schema.get("type") if schema else param.get("type")
+    data_type = _to_py_type(data_type, default=default_type)
+    default_value = schema.get("default", default_value) if schema else param.get("default", default_value)
+    if default_value is not None and data_type == "str":
+        default_value = f"'{default_value}'"
+    return name, py_name, data_type, default_value
+
+
+def main():
+    api = OpenApi("res/openapi.yml", "eocdb.ws")
+    api.gen_code('_openapi_stubs')
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/ws/test_openapi.py
@@ -0,0 +1,19 @@
+import unittest
+
+from eocdb.ws.openapi import OpenApi
+
+
+class OpenApiTest(unittest.TestCase):
+    def test_init(self):
+        api = OpenApi("res/openapi.yml", "eocdb.ws")
+        self.assertIsNotNone(api.spec)
+        self.assertEqual(api.spec.get("openapi"), "3.0.0")
+
+        self.assertIsNotNone(api.schemas)
+        self.assertEqual(8, len(api.schemas))
+
+        self.assertIsNotNone(api.operations)
+        self.assertEqual(21, len(api.operations))
+
+        self.assertIsNotNone(api.path_mappings)
+        self.assertEqual(21, len(api.path_mappings))
