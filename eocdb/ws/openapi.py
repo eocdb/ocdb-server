8ee98eedc0d444ce1dcbded18cfdb7479352b9dd
Norman Fomferra <norman.fomferra@gmail.com>
2018-10-08 18:42:04 +0200

    made the Model class part of ws package

--- eocdb/ws/openapi.py
+++ eocdb/ws/openapi.py
@@ -6,70 +6,6 @@ import yaml
 
 from eocdb.ws.webservice import url_pattern
 
-_MODEL_CODE = '''
-import pprint
-from typing import Any, Dict, Type, TypeVar
-
-T = TypeVar('T')
-
-
-class Model(object):
-
-    @classmethod
-    def from_dict(cls: Type[T], d: Dict[str, Any]) -> T:
-        """Get the dictionary ``d`` as a model."""
-        # TODO deserialize model
-        return cls()
-
-    def to_dict(self) -> Dict[str, Any]:
-        """Get the model properties as a dict."""
-        result = {}
-
-        prop_names = [k for k in self.__dict__ if hasattr(self, k) and hasattr(self, "_" + k)]
-        for prop_name in prop_names:
-            value = getattr(self, prop_name)
-            if isinstance(value, list):
-                result[prop_name] = list(map(self._to_dict, value))
-            elif hasattr(value, "to_dict"):
-                result[prop_name] = value.to_dict()
-            elif isinstance(value, dict):
-                result[prop_name] = dict(map(self._to_dict_pair, value.items()))
-            else:
-                result[prop_name] = value
-
-        return result
-
-    @classmethod
-    def _to_dict(cls, x):
-        return x.to_dict() if hasattr(x, "to_dict") else x
-
-    @classmethod
-    def _to_dict_pair(cls, x):
-        key, value = x
-        if not hasattr(value, "to_dict"):
-            return x
-        return key, value.to_dict()
-
-    def to_str(self) -> str:
-        """Returns the string representation of the model
-
-        :rtype: str
-        """
-        return pprint.pformat(self.to_dict())
-
-    def __repr__(self) -> str:
-        """For `print` and `pprint`"""
-        return self.to_str()
-
-    def __eq__(self, other) -> bool:
-        """Returns true if both objects are equal"""
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other) -> bool:
-        """Returns true if both objects are not equal"""
-        return not self == other
-'''
-
 TAB = "    "
 
 
@@ -237,7 +173,6 @@ class OpenApi:
         for schema in self._schemas:
             schema.gen_code(module_code)
         module_code["__init__"] = ["\n"]
-        module_code['_model'] = [line + "\n" for line in _MODEL_CODE.split("\n")]
         self._write_code(base_dir, package, module_code)
 
     def _gen_handlers(self, base_dir: str):
@@ -269,9 +204,7 @@ class OpenApi:
             code.append(f"class {class_name}(WsRequestHandler):\n")
             for op in ops:
                 func_name = _to_py_lower(op.operation_id)
-                header_params = op.header_parameters
                 path_params = op.path_parameters
-                query_params = op.query_parameters
 
                 path_params_str_parts = []
                 for path_param in path_params:
@@ -450,7 +383,7 @@ class Schema:
         else:
             code = ["from typing import List\n",
                     "\n",
-                    "from ._model import Model\n"]
+                    "from ..model import Model\n"]
             module_code[module_name] = code
 
         code.append("\n")
---
+++ tests/ws/test_model.py
@@ -0,0 +1,147 @@
+import unittest
+from typing import List
+
+from eocdb.ws.model import Model
+
+
+class ModelTest(unittest.TestCase):
+    PERSON_1_DICT = dict(name="Hans-Dieter", age=37, size=182.5, female=False)
+    PERSON_2_DICT = dict(name="Gertrude", age=32, size=167.2, female=True)
+
+    def test_flat_object_to_dict(self):
+        person = Person()
+        person.name = "Hans-Dieter"
+        person.age = 37
+        person.size = 182.5
+        person.female = False
+        actual_dict = person.to_dict()
+        expected_dict = ModelTest.PERSON_1_DICT
+        self.assertEqual(expected_dict, actual_dict)
+
+    def test_flat_object_from_dict(self):
+        actual_person = Person.from_dict(ModelTest.PERSON_2_DICT)
+        expected_person = Person()
+        expected_person.name = "Gertrude"
+        expected_person.age = 32
+        expected_person.size = 167.2
+        expected_person.female = True
+        self.assertEqual(expected_person, actual_person)
+
+    def test_deep_object_to_dict(self):
+        person_1 = Person()
+        person_1.name = "Hans-Dieter"
+        person_1.age = 37
+        person_1.size = 182.5
+        person_1.female = False
+        person_2 = Person()
+        person_2.name = "Gertrude"
+        person_2.age = 32
+        person_2.size = 167.2
+        person_2.female = True
+        query = PersonQuery()
+        query.expr = 'size > 165.0'
+        result = PersonQueryResult()
+        result.query = query
+        result.persons = [person_1, person_2]
+        actual_dict = result.to_dict()
+        expected_dict = dict(query=dict(expr="size > 165.0"),
+                             persons=[ModelTest.PERSON_1_DICT,
+                                      ModelTest.PERSON_2_DICT])
+        self.assertEqual(expected_dict, actual_dict)
+
+    def test_deep_object_from_dict(self):
+        actual_result = PersonQueryResult.from_dict(dict(query=dict(expr="size > 165.0"),
+                                                         persons=[ModelTest.PERSON_1_DICT,
+                                                                  ModelTest.PERSON_2_DICT]))
+        expected_person_1 = Person()
+        expected_person_1.name = "Hans-Dieter"
+        expected_person_1.age = 37
+        expected_person_1.size = 182.5
+        expected_person_1.female = False
+        expected_person_2 = Person()
+        expected_person_2.name = "Gertrude"
+        expected_person_2.age = 32
+        expected_person_2.size = 167.2
+        expected_person_2.female = True
+        expected_query = PersonQuery()
+        expected_query.expr = 'size > 165.0'
+        expected_result = PersonQueryResult()
+        expected_result.query = expected_query
+        expected_result.persons = [expected_person_1, expected_person_2]
+        self.assertEqual(expected_result, actual_result)
+
+
+class Person(Model):
+
+    def __init__(self):
+        self._name = None
+        self._age = None
+        self._size = None
+        self._female = None
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def age(self) -> int:
+        return self._age
+
+    @age.setter
+    def age(self, value: int):
+        self._age = value
+
+    @property
+    def size(self) -> float:
+        return self._size
+
+    @size.setter
+    def size(self, value: float):
+        self._size = value
+
+    @property
+    def female(self) -> bool:
+        return self._female
+
+    @female.setter
+    def female(self, value: bool):
+        self._female = value
+
+
+class PersonQuery(Model):
+    def __init__(self):
+        self._expr = None
+
+    @property
+    def expr(self) -> str:
+        return self._expr
+
+    @expr.setter
+    def expr(self, value: str):
+        self._expr = value
+
+
+class PersonQueryResult(Model):
+    def __init__(self):
+        self._query = None
+        self._persons = None
+
+    @property
+    def query(self) -> PersonQuery:
+        return self._query
+
+    @query.setter
+    def query(self, value: PersonQuery):
+        self._query = value
+
+    @property
+    def persons(self) -> List[Person]:
+        return self._persons
+
+    @persons.setter
+    def persons(self, value: List[Person]):
+        self._persons = value
