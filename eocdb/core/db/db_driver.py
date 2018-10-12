37a84a33fc80a6e0fc6f52ca0029bd20ff808ee1
Norman Fomferra <norman.fomferra@gmail.com>
2018-10-12 10:34:06 +0200

    deprecated methods

--- eocdb/core/db/db_driver.py
+++ eocdb/core/db/db_driver.py
@@ -2,14 +2,17 @@ from abc import abstractmethod
 from typing import List
 
 from eocdb.core import Service, Dataset
+from eocdb.core.deprecated import deprecated
 
 
 class DbDriver(Service):
 
     @abstractmethod
+    @deprecated
     def get(self, query_expression=None) -> List[Dataset]:
         """Query database for list of datasets matching the query_expression"""
 
     @abstractmethod
+    @deprecated
     def insert(self, document):
         """Insert one document into database"""
