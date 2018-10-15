b729a3a2943af0357269be5752bec1906d4271f7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2018-10-15 13:28:03 +0200

    works now(!?)

--- tests/ws/test_main.py
+++ tests/ws/test_main.py
@@ -1,10 +1,11 @@
 import unittest
 
-from eocdb.ws.main import main  # , new_web_service
+import tornado.ioloop
 
-
+from eocdb.ws.main import main, new_web_service  # , new_web_service
 # from tornado.ioloop import IOLoop
 # from eocdb.ws.service import WebService
+from eocdb.ws.webservice import WebService
 
 
 class MainSmokeTest(unittest.TestCase):
@@ -16,10 +17,7 @@ class MainSmokeTest(unittest.TestCase):
             pass
 
     def test_start_stop_service(self):
-        pass
-        # TODO: The following tests code will cause timeouts in tests/test_handlers.py - why?
-        # service = new_web_service(args=['--port', '20001', '--update', '0'])
-        # self.assertIsInstance(service, WebService)
-        # # service.stop()
-        # IOLoop.current().call_later(0.1, service.stop)
-        # service.start()
+        service = new_web_service(args=['--port', '20001', '--update', '0'])
+        self.assertIsInstance(service, WebService)
+        tornado.ioloop.IOLoop.current().call_later(0.1, service.stop)
+        service.start()
