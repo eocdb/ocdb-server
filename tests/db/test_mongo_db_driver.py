0e9ce946d818ac10b89c4d3875e07d36c5c8dac9
tom <tom.block@brockmann-consult.de>
2018-10-17 16:45:42 +0200

    intermediate

--- tests/db/test_mongo_db_driver.py
+++ tests/db/test_mongo_db_driver.py
@@ -2,6 +2,7 @@ import unittest
 
 from eocdb.core.query.parser import QueryParser
 from eocdb.db.mongo_db_driver import MongoDbDriver
+from tests import helpers
 
 
 class TestMongoDbDriver(unittest.TestCase):
@@ -22,18 +23,18 @@ class TestMongoDbDriver(unittest.TestCase):
             pass
 
     def test_insert_one_and_get(self):
-        pass
-        # doc = {"affiliations": "UCSB",
-        #        "received": 20160829,
-        #        "investigators": "Norm_Nelson",
-        #        "records": [{"lon": 109.8, "lat": -38.4, "station": 998, "depth": 20, "sample": 36},
-        #                    {"lon": 109.8, "lat": -38.4, "station": 998, "depth": 20, "sample": 35}]}
-        #
-        # self.driver.insert(doc)
-        #
-        # dataset_list = self.driver.get()
-        # self.assertEqual(1, len(dataset_list))
-        # self.assertEqual("UCSB", dataset_list[0].metadata["affiliations"])
+        dataset = helpers.new_test_dataset(1)
+        dataset.metadata["affiliations"] = "UCSB"
+        dataset.metadata["received"] = "20160829"
+        dataset.metadata["investigators"] = "Norm_Nelson"
+
+        dataset.records = [{"lon": 109.8, "lat": -38.4, "station": 998, "depth": 20, "sample": 36},
+                           {"lon": 109.8, "lat": -38.4, "station": 998, "depth": 20, "sample": 35}]
+
+        id = self.driver.add_dataset(dataset)
+
+        result = self.driver.get_dataset(id)
+        self.assertIsNotNone(result)
 
     def test_insert_two_and_get_by_location(self):
         pass
