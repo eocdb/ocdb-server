2cb2fab989be667c57e216be7b511e83e2f4f250
tom <tom.block@brockmann-consult.de>
2018-12-06 08:26:19 +0100

    resolved conflicts

--- tests/db/test_mongo_query_converter.py
+++ tests/db/test_mongo_query_converter.py
@@ -11,34 +11,34 @@ class TestMongoQueryConverter(unittest.TestCase):
         self.converter = MongoDbDriver.QueryConverter()
 
     def test_to_dict_empty(self):
-        dict = self.converter.to_dict(DatasetQuery())
-        self.assertEqual({}, dict)
+        mongo_dict = self.converter.to_dict(DatasetQuery())
+        self.assertEqual({}, mongo_dict)
 
     def test_to_dict_expression(self):
-        dict = self.converter.to_dict(DatasetQuery(expr="bananas:many"))
-        self.assertEqual({'metadata.bananas': 'many'}, dict)
+        mongo_dict = self.converter.to_dict(DatasetQuery(expr="bananas:many"))
+        self.assertEqual({'metadata.bananas': 'many'}, mongo_dict)
 
     def test_to_dict_region(self):
         query = DatasetQuery(region=[11, -18, 12, -17.4])
-        dict = self.converter.to_dict(query)
+        mongo_dict = self.converter.to_dict(query)
         self.assertEqual({'latitudes': {'$gte': -18, '$lte': -17.4},
-                          'longitudes': {'$gte': 11, '$lte': 12}}, dict)
+                          'longitudes': {'$gte': 11, '$lte': 12}}, mongo_dict)
 
     def test_to_dict_times_start_to_stop(self):
         query = DatasetQuery(time=["2016-01-01T00:00:00", "2016-01-01T04:00:00"])
-        dict = self.converter.to_dict(query)
+        mongo_dict = self.converter.to_dict(query)
         self.assertEqual({'times': {'$gte': datetime.datetime(2016, 1, 1, 0, 0),
-                                    '$lte': datetime.datetime(2016, 1, 1, 4, 0)}}, dict)
+                                    '$lte': datetime.datetime(2016, 1, 1, 4, 0)}}, mongo_dict)
 
     def test_to_dict_times_no_start(self):
         query = DatasetQuery(time=[None, "2016-01-01T04:00:00"])
-        dict = self.converter.to_dict(query)
-        self.assertEqual({'times': {'$lte': datetime.datetime(2016, 1, 1, 4, 0)}}, dict)
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({'times': {'$lte': datetime.datetime(2016, 1, 1, 4, 0)}}, mongo_dict)
 
     def test_to_dict_times_no_stop(self):
         query = DatasetQuery(time=["2016-01-01T00:00:00", None])
-        dict = self.converter.to_dict(query)
-        self.assertEqual({'times': {'$gte': datetime.datetime(2016, 1, 1, 0, 0)}}, dict)
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({'times': {'$gte': datetime.datetime(2016, 1, 1, 0, 0)}}, mongo_dict)
 
     def test_to_dict_both_None(self):
         query = DatasetQuery(time=[None, None])
@@ -51,14 +51,17 @@ class TestMongoQueryConverter(unittest.TestCase):
     def test_to_dict_one_pgroup(self):
         query = DatasetQuery(pgroup=["sal"])
 
-        dict = self.converter.to_dict(query)
-        self.assertEqual({'attributes': {'$in': ['sal']}}, dict)
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({'attributes': {'$in': ['sal']}}, mongo_dict)
 
     def test_to_dict_two_pgroup(self):
         query = DatasetQuery(pgroup=["sal", "Chl"])
 
-        dict = self.converter.to_dict(query)
-        self.assertEqual({'attributes': {'$in': ['sal', 'Chl']}}, dict)
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({'attributes': {'$in': ['sal', 'Chl']}}, mongo_dict)
+
+    def test_to_dict_shallow_no(self):
+        query = DatasetQuery(shallow='no')
 
         mongo_dict = self.converter.to_dict(query)
         self.assertEqual({'metadata.optical_depth_warning': {'$not': {'$eq': 'true'}}}, mongo_dict)
@@ -74,15 +77,15 @@ class TestMongoQueryConverter(unittest.TestCase):
 
         mongo_dict = self.converter.to_dict(query)
         self.assertEqual({'metadata.optical_depth_warning': 'true'}, mongo_dict)
+
     def test_to_dict_mtype_all(self):
         query = DatasetQuery(mtype='all')
 
-        dict = self.converter.to_dict(query)
-        self.assertEqual({}, dict)
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({}, mongo_dict)
 
     def test_to_dict_mtype_brdf(self):
         query = DatasetQuery(mtype='brdf')
 
-        dict = self.converter.to_dict(query)
-        self.assertEqual({'metadata.data_type': 'brdf'}, dict)
-
+        mongo_dict = self.converter.to_dict(query)
+        self.assertEqual({'metadata.data_type': 'brdf'}, mongo_dict)
