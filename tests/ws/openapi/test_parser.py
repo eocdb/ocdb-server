388dbf8eceed7485a24f6def4b3c44431f1e1068
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-05 13:34:29 +0100

    Changed number of components to 23 and path_items to 22 as one each was added to teh API

--- tests/ws/openapi/test_parser.py
+++ tests/ws/openapi/test_parser.py
@@ -17,11 +17,11 @@ class ParserTest(unittest.TestCase):
         self.assertIsNotNone(openapi.components.schemas)
         self.assertEqual(13, len(openapi.components.schemas))
         self.assertIsNotNone(openapi.components.parameters)
-        self.assertEqual(22, len(openapi.components.parameters))
+        self.assertEqual(23, len(openapi.components.parameters))
         self.assertIsNotNone(openapi.components.request_bodies)
         self.assertEqual(7, len(openapi.components.request_bodies))
         self.assertIsNotNone(openapi.components.responses)
         self.assertEqual(13, len(openapi.components.responses))
 
         self.assertIsNotNone(openapi.path_items)
-        self.assertEqual(21, len(openapi.path_items))
+        self.assertEqual(22, len(openapi.path_items))
