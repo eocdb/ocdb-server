950faafdc11fe4bc3a13d9cffe996798fb4f996d
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-01 15:03:01 +0100

    Ensured that pnames are lower case in search

--- ocdb/ws/handlers/_handlers.py
+++ ocdb/ws/handlers/_handlers.py
@@ -622,6 +622,8 @@ class Datasets(WsRequestHandler):
         count = self.query.get_param_int('count', default=None)
         user_id = self.query.get_param_int('user_id', default=None)
 
+        pname = [item.lower() for item in pname]
+
         if self.has_admin_rights():
             status = status
         elif self.has_submit_rights():
