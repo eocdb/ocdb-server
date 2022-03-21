ce363d881a697b65b45c2f6df8d2f406f1dbc9e6
dzelge <helge.dzierzon@brockmann-consult.de>
2022-03-21 10:51:24 +0100

    Fixed security issue allowing imputing code when submitting data using the submission path and submission id during a PUT operation

---
+++ tests/ws/test_utils.py
@@ -0,0 +1,60 @@
+import unittest
+
+from ocdb.ws.errors import WsBadRequestError
+from ocdb.ws.utils import ensure_valid_submission_id, ensure_valid_path
+
+
+class UtilsTest(unittest.TestCase):
+    def test_ensure_valid_submission_id(self):
+        submission_id = "tintin"
+        ensure_valid_submission_id(submission_id)
+
+        submission_id = "tintin\nhelge"
+        ensure_valid_submission_id(submission_id)
+
+        submission_id = "/tintin"
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_submission_id(submission_id)
+
+        self.assertEqual("HTTP 400: Please do not use dots and slashes in your submission id.", str(e.exception))
+
+        submission_id = "./tintin"
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_submission_id(submission_id)
+
+        self.assertEqual("HTTP 400: Please do not use dots and slashes in your submission id.", str(e.exception))
+
+        submission_id = "./tintin\n/."
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_submission_id(submission_id)
+
+        self.assertEqual("HTTP 400: Please do not use dots and slashes in your submission id.", str(e.exception))
+
+    def test_ensure_valid_path(self):
+        path = "test/bla/tintin"
+        ensure_valid_path(path)
+
+        msg = 'HTTP 400: Please provide the path as format: AFFILIATION (acronym)/EXPERIMENT/CRUISE and use ' \
+              'characters, numbers and underscores only.'
+
+        path = "/test/bla/tintin"
+
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_path(path)
+
+        self.assertEqual(msg, str(e.exception))
+
+        path = "/te.st/bla/tintin"
+
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_path(path)
+
+        self.assertEqual(msg, str(e.exception))
+
+        path = "../test/bla/tintin/"
+
+        with self.assertRaises(WsBadRequestError) as e:
+            ensure_valid_path(path)
+
+        self.assertEqual(msg, str(e.exception))
+
