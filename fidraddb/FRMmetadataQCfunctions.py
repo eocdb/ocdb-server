96e674e4939adaba67f55d01258712dc96b0cfd0
uwel <uwe.lange@brockmann-consult.de>
2022-09-12 12:39:28 +0200

    Heloise's script for import check of calibration files

---
+++ fidraddb/FRMmetadataQCfunctions.py
@@ -0,0 +1,130 @@
+### all functions that check that the value for each metadata label is valid
+import re
+
+
+def FRMmetadataQC(metadata, scan, filekey):
+    metadataQC = {}
+    inst = checkinst(metadata)
+    for key in metadata.keys():
+        if key == 'CALDATE':
+            metadataQC[key] = FRMcheckDATE(metadata[key])
+        if key == 'DEVICE':
+            metadataQC[key] = FRMcheckDEVICE(metadata[key])
+        if key == 'CALDATA':
+            if filekey == 'RADCAL':
+                metadataQC[key] = FRMcheckCALDATA(metadata[key], scan, inst)
+            elif filekey in ['POLDATA', 'ANGDATA']:
+                metadataQC[key] = FRMisNCOLdata(metadata[key], key, scan, ncol=5)
+            elif filekey == 'STRAYDATA':
+                metadataQC[key] = FRMisNCOLdata(metadata[key], key, scan, ncol=257)
+            elif filekey == 'TEPDATA':
+                metadataQC[key] = FRMisNCOLdata(metadata[key], key, scan, ncol=3)
+        if key in ['PANEL_ID', 'LAMP_ID', 'USER', 'CALLAB']:
+            metadataQC[key] = FRMisempty(metadata[key])
+        if key in ['AMBIENT_TEMP', 'LAMP_CCT', 'VERSION', 'POLANGLE', 'REFTEMP']:
+            metadataQC[key] = FRMisfloat(metadata[key])
+        if key in ['PANELDATA', 'LAMPDATA']:
+            metadataQC[key] = FRMisNCOLdata(metadata[key], key, scan, ncol=4)
+    return(metadataQC)
+
+def checkinst(metadata):
+    try:
+        dev = metadata['DEVICE']
+        code_inst = dev[0:3]
+        if code_inst in ["SAM", "SAT"]:
+            return code_inst
+        else:
+            return "unknown"
+    except ValueError:
+        return "unkown"
+
+def FRMcheckDATE(strdate):
+    res = "not valid"
+    if len(strdate) == 19:
+        year = strdate[0:4]
+        month = strdate[5:7]
+        day = strdate[8:10]
+        hour = strdate[11:13]
+        min = strdate[14:16]
+        sec = strdate[17:19]
+
+        Yqc = re.search(r'\b(?:2030|20[0-4][0-9]|19[789][0-9])\b', year)
+        MOqc = re.search(r'\b(?:12|11|10|0[1-9])\b', month)
+        Dqc = re.search(r'\b(?:31|30|[1-2][0-9]|0[1-9])\b', day)
+        Hqc = re.search(r'\b(?:2[0-3]|[0-1][0-9])\b', hour)
+        MIqc = re.search(r'\b(?:[0-5][0-9])\b', min)
+        Sqc = re.search(r'\b(?:[0-5][0-9])\b', sec)
+
+        allQC = [Yqc, MOqc, Dqc, Hqc, MIqc, Sqc]
+        if all(allQC):
+            res = "valid"
+    return(res)
+
+
+
+def FRMcheckDEVICE(dev):
+    res = "not valid"
+    if len(dev) >= 7:
+        code_inst = dev[0:3]
+        devQC = None
+        if code_inst == 'SAM':
+            devQC = re.search(r'\b(?:\_[0-9][0-9][0-9][0-9])\b', dev[3:8])
+        if code_inst == 'SAT':
+            devQC = re.search(r'\b(?:[0-9][0-9][0-9][0-9])\b', dev[3:7])
+    if devQC :
+        res = 'valid'
+    return(res)
+
+
+def is_valid_float(element: str) -> bool:
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+def FRMisempty(element: str) -> str:
+    if element and is_valid_float(element) == False:
+        return 'valid'
+    else:
+        return 'not valid'
+
+def FRMisfloat(element: str) -> str:
+    if is_valid_float(element) :
+        return 'valid'
+    else :
+        return 'not valid'
+
+def FRMcheckCALDATA(caldata, scan, inst):
+    # split lines, trim and remove leading slash
+    line1 = re.split("[\t]+",caldata)
+    isfloat = [is_valid_float(el) for el in line1]
+    res = "not valid"
+    try:
+        istart = scan.index('[CALDATA]')
+        iend = scan.index('[END_OF_CALDATA]')
+        Nlines = iend - istart
+    except:
+        Nlines = 0
+    if Nlines > 5 & all(isfloat):
+        if  inst == 'SAM' and len(line1) == 8:
+        ## check TriOS instrument
+            res = 'valid'
+        elif inst == 'SAT' and len(line1) == 10:
+            res = 'valid'
+    return(res)
+
+
+def FRMisNCOLdata(line, key, scan, ncol=4):
+    line1 = re.split("[\t]+",line)
+    isfloat = [is_valid_float(el) for el in line1]
+    res = "not valid"
+    try:
+        istart = scan.index('['+key+']')
+        iend = scan.index('[END_OF_'+key+']')
+        Nlines = iend - istart
+    except:
+        Nlines = 0
+    if Nlines > 5 and all(isfloat) and len(line1) == ncol:
+       res = 'valid'
+    return res
---
+++ fidraddb/main.py
@@ -0,0 +1,54 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from FRMmetadataQCfunctions import FRMmetadataQC
+from FRMmainfunctions import readFRMfile, getfilekey, readFRMmetadata, checkFRMmetadata
+import shutil
+
+## directory for non-checked FRM4SOC files /// To be adapted ///
+DIR_uploaded_files = "/FRM4SOC/fileformats/"
+
+#/// To be adapted ///
+file = DIR_uploaded_files+"filename.txt"
+
+def check_FRM_uploaded_file(file):
+    # read the file and scann all lines
+    scan = readFRMfile(file)
+
+    # recognize file keywords
+    filekey = getfilekey(scan)
+
+    # read all metadata info
+    metadata = readFRMmetadata(scan)
+
+    print('check metadata validity:')
+# apply QC on metadata
+    metadataQC = FRMmetadataQC(metadata, scan, filekey)
+
+    for k in metadataQC.keys():
+        print(k + ': ' + metadataQC[k])
+    print('------------------------')
+
+## define dictionnary with for each file type which are mandatory and not mandatory metadata
+    MANDATORY = {'RADCAL' : ['CALDATE', 'CALLAB', 'DEVICE', 'CALDATA'],
+             'ANGDATA' : ['DEVICE', 'CALDATA', 'CALDATE', 'CALLAB'],
+             'POLDATA' : ['CALDATE', 'CALLAB', 'DEVICE', 'CALDATA', 'POLANGLE'],
+             'STRAYDATA': ['CALDATE', 'CALLAB', 'DEVICE', 'CALDATA'],
+             'TEMPDATA' : ['CALDATE', 'CALLAB', 'DEVICE', 'CALDATA', 'REFTEMP']}
+
+    OPTIONAL = {'RADCAL' : ['PANELDATA', 'LAMPDATA', 'LAMP_CCT', 'VERSION', 'USER', 'LAMP_ID', 'PANEL_ID'],
+            'ANGDATA' : ['VERSION', 'USER'],
+            'POLDATA': ['VERSION', 'USER'],
+            'STRAYDATA': ['VERSION', 'USER'],
+            'TEMPDATA' : ['VERSION', 'USER']}
+
+    OK = checkFRMmetadata(filekey, metadataQC, MANDATORY, OPTIONAL)
+
+    ## directory for checked FRM4SOC files (files are copied and file names are adapted) /// To be adapted ///
+    DIR_accepted_files = "/FRM4SOC/file_QC_OK/"
+
+    if OK:
+    shutil.copy(file ,DIR_accepted_files+filekey+'_'+metadata['DEVICE']+'_'+metadata['CALDATE'][0:10]+'.csv')
+    print("uploaded file passed all QC on file format and is accepted!")
